87de4b6a4d772f8ca897b598fa71797405e5bb67
VidhanshiJain2321 <vidhanshijain2321@gmail.com>
2026-03-26 10:24:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Dummy user data
+users = {"admin": "1234", "user": "abcd"}
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    
+    if username in users and users[username] == password:
+        return f"Welcome {username}!"
+    else:
+        return "Invalid credentials, try again."
+
+if __name__ == "__main__":
+    app.run(debug=True)
